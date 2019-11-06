caf12f90a06cf3061f6c866c85b48705e36c1963
a18joszamper <a18joszamper@inspedralbes.cat>
2019-11-06 19:50:03 +0100

    Primeros 5 puntos, Josue

--- afegeix-usuari-local.sh
+++ afegeix-usuari-local.sh
@@ -1,1 +1,22 @@
 #!bin/var
+
+# 1-Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -eq 0 ]]
+then
+
+  # 2-Get the username (login).
+  read -p 'Introduce tu nombre de usuario: ' username
+
+  # 3-Get the real name (contents for the description field).
+  read -p 'Introduce tu nombre completo: ' fullname
+
+  # 4-Get the password.
+  read -s -p 'Introduce tu contraseña: ' password
+
+  # 5-Create the user.
+  useradd -c "${fullname}" -m ${username}
+
+
+else
+  echo 'No tienes permisos.'
+fi
